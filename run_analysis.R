293e4ce888d276cd5918f4f63417c6730f3d9c91
Kim <MANKIMV@CORP.JTI.COM>
2018-12-24 21:59:41 +0000

    new run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+setwd("./ProgrAssignCourse3Week4/UCI HAR Dataset/")
+train_var_df<-read.fwf(file="./train/X_train.txt",
+  widths=c(16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16))
+train_obs_df<-read.fwf(file="./train/y_train.txt", widths = 1)
+train_subj_df<-read.fwf(file="./train/subject_train.txt", widths = 2)
+train_df<-cbind(train_obs_df, train_var_df)
+train_df<-cbind(train_subj_df, train_df)
+test_var_df <- read.fwf(file="./test/X_test.txt",
+  widths=c(17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16))
+test_obs_df<-read.fwf(file="./test/y_test.txt", widths = 1)
+test_subj_df<-read.fwf(file="./test/subject_test.txt", widths = 2)
+test_df<-cbind(test_obs_df, test_var_df)
+test_df<-cbind(test_subj_df, test_df)
+consol_df<-rbind(train_df, test_df)# this step completes task1
+#task 2:
+features<-read.delim(file="features.txt", header = FALSE, sep="")
+ms<-grep("mean\\()|std\\()", features[,2])#here I've created the list of relevant column numbers - 
+                                          # - any variable having mean() or std() - see read.me as to why
+mean_std_df<-subset(consol_df, select=c(1,2, ms+2))#ms+2 caters for the shift (+2) resulting from
+                                                  #left bound columns containing 'subject' and 'activity' 
+#task 3
+rownames_df <- read.fwf(file="./activity_labels.txt", widths=c(2, 18))
+rownames_df$V2 <- lapply(rownames_df$V2, as.character)#important as V2 are factors now;
+                                                      #if not done, next line returns just numbers, not meaningful text
+  for (i in 1:nrow(mean_std_df)) {
+    mean_std_df[i,2]<-rownames_df[mean_std_df[i,2],2]
+  }
+#task 4
+features$V2 <- lapply(features$V2, as.character)#again, as with rownames_df$V2 case, features$V2 are factors now;
+                                                #if not done, next line returns just numbers, not meaningful text
+  #step 1 - improve descriptiveness of the variable names, like eliminate '(), replace 'Gyro' with 'angular velocity' etc
+cl<-c()
+for (i in 1:length(ms)) cl[i]<-features[ms[i],2]#copying mean and std variables (known from ms) from 'features' to 'cl'
+unlist(cl)
+  for (i in 1:length(cl)){
+    cl[i]<-gsub("\\(|)", "", cl[i])
+    cl[i]<-sub("^t", "time domain: ", cl[i])
+    cl[i]<-sub("^f", "frequency domain: ", cl[i])
+    cl[i]<-sub("std", "standard deviation", cl[i])
+    cl[i]<-sub("Acc", " acceleration", cl[i])
+    cl[i]<-sub("Gyro", " angular velocity", cl[i])
+    cl[i]<-sub("Jerk", " jerk signal", cl[i])
+    cl[i]<-sub("Mag", " magnitude", cl[i])
+    cl[i]<-sub("X", "along X axis", cl[i])
+    cl[i]<-sub("Y", "along Y axis", cl[i])
+    cl[i]<-sub("Z", "along Z axis", cl[i])
+  }
+  #step 2 - copy cl descriptions as variable names of mean_std_df
+for (i in 1:length(cl)) names(mean_std_df)[i+2]<-cl[i]#i+2 caters for 2 leftbound columns that will be renamed at step 3 below
+  #step 3 - rename 1st 2 columns
+names(mean_std_df)[1]<-"subjects"
+names(mean_std_df)[2]<-"activity"
+#task 5
+library(dplyr)
+group_sub<-group_by(mean_std_df, subjects)#step1 - grouping by subjects
+group_sub_act<-group_by(group_sub, activity, add=TRUE)#step2 - adding grouping by activity
+summ<-summarise_all(group_sub_act, funs(mean))
+for (i in 3:(length(ms)+2)) names(summ)[i]<-paste0(names(summ)[i], "-mean" )# adding 'mean' to the column names
